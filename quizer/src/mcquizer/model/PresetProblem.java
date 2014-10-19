f580ed61a454a1c0a030613d2d7bef2e4cbe9786
knary <knary@users.noreply.github.com>
2014-10-19 17:31:16 -0400

    Revert "Removed test helper for non-existing test"
    This reverts commit 0a7da38a55845f825c9bee4e507bad6ea4a06eb5.

---
+++ quizer/src/mcquizer/model/PresetProblem.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+
+package mcquizer.model;
+
+import mcquizer.model.interfaces.IQaPair;
+
+/**
+ * A problem with a single constant question and answer provided at
+ * instantiation.
+ * 
+ * @author Konstantin Naryshkin
+ */
+public class PresetProblem implements IQaPair
+{
+	/**
+	 * The answer for the problem
+	 */
+	private String answer;
+	
+	/**
+	 * The question of this problem
+	 */
+	private String question;
+	
+	/**
+	 * The score, representing the confidence of the software that the user know
+	 * the answer. A lower score represents better knowledge.
+	 * 
+	 * @see #getScore()
+	 */
+	private double score;
+	
+	/**
+	 * Create a new problem instance
+	 * 
+	 * @param myQuestion The question that the problem asks
+	 * @param myAnswer The correct answer to the problem
+	 * @param myScore The score that this
+	 */
+	PresetProblem(String myQuestion, String myAnswer, int myScore)
+	{
+		this.question = myQuestion;
+		this.answer = myAnswer;
+		this.score = myScore;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.magku.quizer.model.IQuestion#getQuestion()
+	 */
+	@Override
+	public String getQuestion()
+	{
+		return this.question;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.magku.quizer.model.IQuestion#getAnswer()
+	 */
+	@Override
+	public String getAnswer()
+	{
+		return this.answer;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.magku.quizer.model.IQuestion#changeScore(int)
+	 */
+	@Override
+	public double getScore()
+	{
+		return this.score;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.magku.quizer.model.IQuestion#changeScore(int)
+	 */
+	@Override
+	public void changeScore(double delta)
+	{
+		this.score += delta;
+		if (this.score <= 0)
+		{
+			this.score = 1;
+		}
+	}
+}
