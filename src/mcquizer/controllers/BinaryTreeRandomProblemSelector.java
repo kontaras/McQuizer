c65b94850a75b1a98e3604b07bf61e1f70af3478
knary <knary@users.noreply.github.com>
2017-10-15 22:16:22 -0400

    Added proper binary selection

--- src/mcquizer/controllers/BinaryTreeRandomProblemSelector.java
+++ src/mcquizer/controllers/BinaryTreeRandomProblemSelector.java
@@ -1,3 +1,4 @@
+
 package mcquizer.controllers;
 
 import java.util.ArrayList;
@@ -12,25 +13,30 @@ import mcquizer.model.interfaces.ISelectable;
  * @author Konstantin Naryshkin
  * @param <T> The type of problem that the the selector is selecting
  */
-public class BinaryTreeRandomProblemSelector<T extends ISelectable> implements IProblemSelector<T>
+public class BinaryTreeRandomProblemSelector<T extends ISelectable>
+		implements IProblemSelector<T>
 {
-	private static class BinaryNode implements ISelectable {
+	private static class BinaryNode implements ISelectable
+	{
 		ISelectable left;
+		
 		ISelectable right;
+		
 		double weight;
 		
-		public BinaryNode(ISelectable l, ISelectable r, double w) {
+		public BinaryNode(ISelectable l, ISelectable r, double w)
+		{
 			this.left = l;
 			this.right = r;
 			this.weight = w;
 		}
-
+		
 		@Override
 		public void changeWeight(double delta)
 		{
 			this.weight += delta;
 		}
-
+		
 		@Override
 		public double getWeight()
 		{
@@ -41,7 +47,7 @@ public class BinaryTreeRandomProblemSelector<T extends ISelectable> implements I
 		{
 			return left;
 		}
-
+		
 		public ISelectable getRight()
 		{
 			return right;
@@ -50,15 +56,14 @@ public class BinaryTreeRandomProblemSelector<T extends ISelectable> implements I
 	
 	/** The problems that we can be picked from */
 	private final List<T> problems;
-
+	
 	/** The random number generator to use */
 	private final Random rng;
 	
 	private final ISelectable root;
-
+	
 	/**
-	 * @param problems
-	 *            Problems to select from
+	 * @param problems Problems to select from
 	 */
 	public BinaryTreeRandomProblemSelector(List<T> problems)
 	{
@@ -67,22 +72,47 @@ public class BinaryTreeRandomProblemSelector<T extends ISelectable> implements I
 		
 		this.root = buildSelectionTree(this.problems);
 	}
-
+	
+	@SuppressWarnings("unchecked") // We know that current can either be a
+																	// BinaryNode or T. If it is not a
+																	// BinaryNode...
 	@Override
 	public T getNextProblem()
 	{
-		// TODO: Implement properly
-
-		return this.problems.get(this.rng.nextInt(this.problems.size()));
+		double rand = this.rng.nextDouble() * this.root.getWeight();
+		
+		ISelectable current = this.root;
+		
+		while (current instanceof BinaryNode)
+		{
+			BinaryNode node = (BinaryNode) current;
+			if (node.getLeft().getWeight() >= rand)
+			{
+				current = node.getLeft();
+			}
+			else
+			{
+				rand -= node.getLeft().getWeight();
+				current = node.getRight();
+			}
+		}
+		
+		return (T) current;
 	}
-
-	private static <T extends ISelectable> ISelectable buildSelectionTree(List<T> problems) {
+	
+	private static <T extends ISelectable> ISelectable
+			buildSelectionTree(List<T> problems)
+	{
 		List<ISelectable> currentLayer = new ArrayList<>(problems);
 		List<ISelectable> nextLayer;
-		while(currentLayer.size() > 1) {
-			nextLayer = new ArrayList<>(problems.size()/2 + 1);
-			for (int i = 0; i <= problems.size() - 1; i +=2) {
-				nextLayer.add(new BinaryNode(currentLayer.get(i), currentLayer.get(i + 1), currentLayer.get(i).getWeight() + currentLayer.get(i + 1).getWeight()));
+		while (currentLayer.size() > 1)
+		{
+			nextLayer = new ArrayList<>(problems.size() / 2 + 1);
+			for (int i = 0; i <= problems.size() - 1; i += 2)
+			{
+				nextLayer.add(new BinaryNode(currentLayer.get(i),
+						currentLayer.get(i + 1), currentLayer.get(i).getWeight()
+								+ currentLayer.get(i + 1).getWeight()));
 			}
 			
 			currentLayer = nextLayer;
